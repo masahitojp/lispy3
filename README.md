68704a8723f5e80883ee3864f0dc73ae334b7cdb
masahitojp <randomstep@gmail.com>
2019-08-19 00:25:49 +0900

    feat: :sparkles: add lispy.py and using prompt-toolkit

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Lispy for Python3.7
 Please Read -> https://norvig.com/lispy.html
+
+# How to use
+
+Run following command
+
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,34 @@
+from prompt_toolkit import PromptSession
+from prompt_toolkit.lexers import PygmentsLexer
+from prompt_toolkit.completion import WordCompleter
+from pygments.lexers.lisp import SchemeLexer
+
+from lispy import eval, parse, to_string, eof_object
+
+
+def main(prompt="lispy> "):
+    scheme_completer = WordCompleter(["begin", "call/cc"])
+    session = PromptSession(
+        lexer=PygmentsLexer(SchemeLexer), completer=scheme_completer
+    )
+
+    while True:
+        try:
+            text = session.prompt(prompt)
+            x = parse(text)
+            if x is eof_object:
+                return
+            val = eval(x)
+            if val is not None:
+                print(to_string(val))
+        except KeyboardInterrupt:
+            continue
+        except EOFError:
+            break
+        except Exception as e:
+            print(f"{type(e).__name__}: {e}")
+    print("GoodBye!")
+
+
+if __name__ == "__main__":
+    main()
